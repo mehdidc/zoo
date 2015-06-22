05faa88c0ce508d3a513db2cf5410a13e9084416
Mehdi Cherti <mehdicherti@llrgrcl01.in2p3.fr>
2015-06-22 19:04:40 +0200

    vgg

--- vgg.py
+++ vgg.py
@@ -202,6 +202,7 @@ def build_model_small(input_width=32, input_height=32, output_dim=100,
     )
 
     l_hidden1_dropout = lasagne.layers.DropoutLayer(l_hidden1, p=0.5)
+    #l_hidden1_dropout = l_hidden1
 
     l_hidden2 = lasagne.layers.DenseLayer(
         l_hidden1_dropout,
@@ -209,6 +210,8 @@ def build_model_small(input_width=32, input_height=32, output_dim=100,
         nonlinearity=lasagne.nonlinearities.rectify,
     )
     l_hidden2_dropout = lasagne.layers.DropoutLayer(l_hidden2, p=0.5)
+     #l_hidden2_dropout = l_hidden1_dropout
+
     l_out = lasagne.layers.DenseLayer(
         l_hidden2_dropout,
         num_units=output_dim,
@@ -238,7 +241,7 @@ if __name__ == "__main__":
             return status
  
     batch_optimizer = MyBatchOptimizer(verbose=1, max_nb_epochs=100, batch_size=BATCH_SIZE,
-                                       optimization_procedure=(updates.rmsprop, {"learning_rate":0.01}))
+            optimization_procedure=(updates.momentum, {"learning_rate":1e-1, "momentum": 0.8}))
     nnet = NeuralNet(model,
                      X_type=T.tensor4,
                      batch_optimizer=batch_optimizer)
@@ -249,6 +252,5 @@ if __name__ == "__main__":
     X = data.X.reshape((data.X.shape[0], 3, 32, 32))
     y = data.y
     X, y = shuffle(X, y)
-    print(X.max())
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     nnet.fit(X_train, y_train)
